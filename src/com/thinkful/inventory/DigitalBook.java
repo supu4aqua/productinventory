a022c266adfcc598ee03332a867bbf6f83fce2e4
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-06-10 10:46:08 -0400

    Add DigitalBook class

---
+++ src/com/thinkful/inventory/DigitalBook.java
@@ -0,0 +1,68 @@
+package com.thinkful.inventory;
+
+import java.util.Date;
+
+public class DigitalBook extends DigitalProduct{
+    private int numberPages;
+    private String author;
+    private String publisher;
+    private Date publishDate;
+
+    public DigitalBook() {
+        this("No name", "No description", 0);
+    }
+
+    public DigitalBook(String title, String description, double basePrice) {
+        this(title, description, basePrice,
+                1, "https://");
+    }
+
+    public DigitalBook(String title, String description, double basePrice,
+                       double fileSize, String downloadLink) {
+        this(title, description, basePrice,
+                fileSize, downloadLink, 1, "None", "None", null);
+    }
+
+    public DigitalBook(String title, String description, double basePrice,
+                       double fileSize, String downloadLink, int numberPages,
+                       String author, String publisher, Date publishDate) {
+        super(title, description, basePrice, fileSize, downloadLink);
+        this.setNumberPages(numberPages);
+        this.setAuthor(author);
+        this.setPublisher(publisher);
+        this.setPublishDate(publishDate);
+    }
+
+    public int getNumberPages() {
+        return numberPages;
+    }
+
+    public void setNumberPages(int numberPages) {
+        this.numberPages = numberPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+}
+
