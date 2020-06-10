5d4bc035afbae9745969ce8b0ca4f4405ffb0cfd
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-06-10 10:33:40 -0400

    Initialise project, add Product super class

---
+++ src/com/thinkful/inventory/Product.java
@@ -0,0 +1,59 @@
+package com.thinkful.inventory;
+
+public class Product {
+    private String title;
+    private String description;
+    private double basePrice;
+
+    public Product() {
+        this("Not set", "None given", 0);
+        System.out.println("No-arg constructor of Product");
+    }
+
+    public Product(String title, String description, double basePrice) {
+        this.setTitle(title);
+        this.setDescription(description);
+        this.setBasePrice(basePrice);
+        System.out.println("3-arg constructor of Product");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return product.getBasePrice() == getBasePrice() &&
+                getTitle().equals(product.getTitle()) &&
+                getDescription().equals(product.getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product{title='%s', basePrice=$%.2f}",
+                getTitle(), getBasePrice());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+}
